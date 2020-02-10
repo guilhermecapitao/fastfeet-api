c2049762a70b2de6fe9c51dbb170cd2a3baddeb4
guilhermecapitao <guilherme.capitao95@gmail.com>
2020-02-10 14:07:55 -0300

    chore: Validação dos dados de entrada

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
@@ -5,6 +6,24 @@ class RecipientController {
     const { name, address } = req.body;
 
     const { street, house_number, complement, state, city, zip_code } = address;
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      address: Yup.object().shape({
+        street: Yup.string().required(),
+        house_number: Yup.string().required(),
+        complement: Yup.string(),
+        state: Yup.string().required(),
+        city: Yup.string().required(),
+        zip_code: Yup.string()
+          .required()
+          .min(8)
+      })
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails ' });
+    }
 
     const recipient = await Recipient.create({
       name,
