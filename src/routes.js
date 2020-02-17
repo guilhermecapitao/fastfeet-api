747ccffcd2606aedd77b1d1e6af32d0185ea4aa8
guilhermecapitao <guilherme.capitao95@gmail.com>
2020-02-17 15:37:06 -0300

    feat: Upload de arquivo

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 // import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(authMiddleware);
 routes.post('/recipients', RecipientController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 // routes.put('/users', UserController.update);
 
